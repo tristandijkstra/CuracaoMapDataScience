fc75b6e4120f46a69635b7b340c7ea88c835a14d
Tristan Dijkstra <tristan.dijkstra@gmail.com>
2022-07-14 12:43:18 -0400

    update misc data

---
+++ misc.py
@@ -0,0 +1,65 @@
+parties2021 = [
+    "UKPK",
+    "VISHON",
+    "KdNT",
+    "MP",
+    "TPK",
+    "DP",
+    "KEM",
+    "MKP",
+    "PNP",
+    "PAR",
+    "PIN",
+    "MAN",
+    "MFK",
+    "PS",
+    "KUMUN",
+]
+
+parties2017 = [
+    "KdNT",
+    "Hustu",
+    "PIN",
+    "PAR",
+    "MAN",
+    "PNP",
+    "FOL-PAN",
+    "MFK",
+    "PS",
+    "MKP",
+    "MP",
+]
+
+parties2017Colours = {
+    "KdNT":"lightgrey",
+    "Hustu":"purple",
+    "PIN":"lightblue",
+    "PAR":"yellow",
+    "MAN":"darkblue",
+    "PNP":"green",
+    "FOL-PAN":"orange",
+    "MFK":"#FFF5EE",
+    "PS":"beige",
+    "MKP":"darkgrey",
+    "MP":"magenta"
+}
+
+parties2021Colours = {
+    "MFK":"#FFF5EE",
+    "PAR":"yellow",
+    "PNP":"green",
+    "MAN":"darkblue",
+    "KEM":"#ffd700",
+    "TPK":"brown",
+    "UKPK":"#DAA520",
+    "PIN":"lightblue",
+    "VISHON":"black",
+    "KdNT":"lightgrey",
+    "MKP":"darkgrey",
+    "DP":"red",
+    "KUMUN":"orange",
+    "Hustu":"purple",
+    "MP":"magenta",
+    "PS":"beige",
+}
+
