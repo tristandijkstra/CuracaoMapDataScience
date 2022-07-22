3bd5e65976204bfa6f2ca43ee303516b0fd8e734
Tristan Dijkstra <tristan.dijkstra@gmail.com>
2022-07-22 13:16:02 -0400

    Updated misc with 2016, 2012 and 2010 data

--- misc.py
+++ misc.py
@@ -29,20 +29,42 @@ parties2017 = [
     "MKP",
     "MP",
 ]
+parties2016 = [
+    "MFK",
+    "MKP",
+    "UKH",
+    "MAN",
+    "KdNT",
+    "PAIS",
+    "MP",
+    "PAS",
+    "DP",
+    "PAR",
+    "PK",
+    "PS",
+    "PNP",
+]
 
-parties2017Colours = {
-    "KdNT":"lightgrey",
-    "Hustu":"purple",
-    "PIN":"lightblue",
-    "PAR":"yellow",
-    "MAN":"darkblue",
-    "PNP":"green",
-    "FOL-PAN":"orange",
-    "MFK":"#FFF5EE",
-    "PS":"beige",
-    "MKP":"darkgrey",
-    "MP":"magenta"
-}
+parties2012 = [
+    "MFK",
+    "FOL",
+    "PS",
+    "MAN",
+    "PAR",
+    "DP",
+    "PAIS",
+    "PNP",
+]
+parties2010 = [
+    "PS",
+    "PPE",
+    "PNP",
+    "PAR",
+    "MSI",
+    "DP",
+    "FOL",
+    "MAN-NPA-FK",
+]
 
 parties2021Colours = {
     "MFK":"#FFF5EE",
@@ -58,8 +80,58 @@ parties2021Colours = {
     "MKP":"darkgrey",
     "DP":"red",
     "KUMUN":"orange",
-    # "Hustu":"purple",
     "MP":"magenta",
     "PS":"beige",
 }
 
+parties2017Colours = {
+    "KdNT":"lightgrey",
+    "Hustu":"purple",
+    "PIN":"lightblue",
+    "PAR":"yellow",
+    "MAN":"darkblue",
+    "PNP":"green",
+    "FOL-PAN":"orange",
+    "MFK":"#FFF5EE",
+    "PS":"beige",
+    "MKP":"darkgrey",
+    "MP":"magenta"
+}
+
+parties2016Colours = {
+    "MFK":"FFF5EE",
+    "MKP":"grey",
+    "UKH":"purple",
+    "MAN":"darkblue",
+    "KdNT":"lightgrey",
+    "PAIS":"lightblue",
+    "MP":"magenta",
+    "PAS":"orange",
+    "DP":"red",
+    "PAR":"yellow",
+    "PK":"brown",
+    "PS":"beige",
+    "PNP":"green",
+}
+
+parties2012Colours = {
+    "MFK":"FFF5EE",
+    "FOL":"orange",
+    "PS":"beige",
+    "MAN":"darkblue",
+    "PAR":"yellow",
+    "DP":"red",
+    "PAIS":"lightblue",
+    "PNP":"green",
+}
+
+parties2010Colours = {
+    "PS":"beige",
+    "PPE":"LightSeaGreen",
+    "PNP":"green",
+    "PAR":"yellow",
+    "MSI":"LimeGreen",
+    "DP":"red",
+    "FOL":"orange",
+    "MAN-NPA-FK":"darkblue",
+}
